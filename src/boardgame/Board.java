0a18070df717a07cd5c7f11b2d7ddb4060993b05
yukioz <yukio.link@gmail.com>
2020-08-12 18:06:08 -0300

    Ajuste na classe Board e criação de Exceções

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -12,6 +12,11 @@ public class Board {
 	//Construtores
 	
 	public Board(int rows, int columns) {
+		
+		//Exception
+		if(rows < 1 || columns < 1) {
+			throw new BoardException("Error creating the board: there must be at least 1 row and 1 column.");
+		}
 		this.rows = rows;
 		this.columns = columns;
 		//Diz quantos quadrados terá a matriz do tabuleiro
@@ -24,33 +29,62 @@ public class Board {
 		return rows;
 	}
 
-	public void setRows(int rows) {
-		this.rows = rows;
-	}
-
 	public int getColumns() {
 		return columns;
 	}
 
-	public void setColumns(int columns) {
-		this.columns = columns;
-	}
 	
 	//Métodos
 	
 	public Piece piece (int row, int column) {
-
+		
+		//Exception
+		if(!positionExist(row, column)) {
+			throw new BoardException("Position not on the Board");
+		}
+		
 		return pieces[row][column];
 	}
 	public Piece piece (Position position) {
+		
+		//Exception
+		if(!positionExist(position)) {
+			throw new BoardException("Position not on the Board");
+		}
 		
 		return pieces[position.getRow()][position.getColumn()];
 	}
 	
 	public void placePiace (Piece piece, Position position) {
 		
+		//Exception
+		if(thereIsAPiece(position)) {
+			throw new BoardException("There is already a piece on position: " + position);
+		}
+		
 		pieces[position.getRow()][position.getColumn()] = piece;
 		piece.position = position;
+	}
+	
+	//Testa posição válida dentro do tabuleiro
+	public boolean positionExist(int row, int column) {
+		
+		return row >= 0 && row < rows && column >= 0 && column < columns;
+	}
+	public boolean positionExist(Position position) {
+		
+		return positionExist(position.getRow(), position.getColumn());
+	}
+	
+	//Testa se já existe uma peça naquela posição
+	public boolean thereIsAPiece(Position position) {
+		
+		//Exception
+		if(!positionExist(position)) {
+			throw new BoardException("Position not on the Board");
+		}
+		
+		return piece(position) != null;
 	}
 	
 }
